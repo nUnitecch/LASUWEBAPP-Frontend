ffed4034a4dc9c0bf66fb52bd8591c9a36daae8a
Cleve-keem <belloharyourmidey@gmail.com>
2025-11-23 12:42:47 -0600

    fix: changed tailwind directive to theme from theme line and also added Header component to the app page and Logo component to the Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "./_components/header";
+
 export default function Home() {
   return (
     <div className="bg-background text-center">
-      <h1 className="font-semibold text-signup-background">
-        Lasu Recommendation App
-      </h1>
+      <Header />
+      {/* <h1 className="font-semibold text-logo-title">Lasu Recommendation App</h1> */}
     </div>
   );
 }
