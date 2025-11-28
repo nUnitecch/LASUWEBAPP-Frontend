f8bc8e6eea26a6ad203ededae9cf1dabadc775ec
Cleve-keem <belloharyourmidey@gmail.com>
2025-11-28 08:16:04 -0600

    fix: resolve the flex-row-reverse issue

--- app/_components/Home/aboutCard.tsx
+++ app/_components/Home/aboutCard.tsx
@@ -6,18 +6,13 @@ type AboutCardType = {
     title: string;
     desc: string;
     image: string;
-    direction?: string;
   };
 };
 
 export default function AboutCard({ info }: AboutCardType) {
-  const descriptionBackground =
-    info.direction === "row-reverse" ? "btn-primary" : "";
   return (
-    <div className={`flex gap-[11.33px] flex-${info.direction}`}>
-      <div
-        className={`w-[179.07px] h-[118.04px] flex-1 rounded-[5.85px] overflow-hidden p-2 shadow-md ${descriptionBackground}`}
-      >
+    <div className="about-card flex gap-[11.33px]">
+      <div className="card-desc w-[179.07px] h-[118.04px] flex-1 rounded-[5.85px] overflow-hidden p-2 shadow-md">
         <div>
           <h2 className="text-[17px] font-bold mb-5">{info.title}</h2>
           <p className="text-[8.41px] leading-[14.5px]">{info.desc}</p>
--- app/data/about.ts
+++ app/data/about.ts
@@ -3,12 +3,10 @@ export const aboutCardInfo = [
     title: "Our Mission",
     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ducimus commodi, officia nemo porro placeat soluta obcaecati corporis maiores autu.",
     image: "heroImg.jpg",
-    direction: "row",
   },
   {
     title: "Our Vission",
     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ducimus commodi, officia nemo porro placeat soluta obcaecati corporis maiores autu.",
     image: "heroImg.jpg",
-    direction: "row-reverse",
   },
 ];
