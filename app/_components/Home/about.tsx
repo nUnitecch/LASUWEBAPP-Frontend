bcea1758bd32ed6489b1127a5b17711fbee4d4ef
Cleve-keem <belloharyourmidey@gmail.com>
2025-11-28 07:56:31 -0600

    chore: made each about card a component and passed the info as a prop

--- app/_components/Home/about.tsx
+++ app/_components/Home/about.tsx
@@ -1,31 +1,14 @@
-import Image from "next/image";
+import AboutCard from "./aboutCard";
+import { aboutCardInfo } from "@/app/data/about";
 
 export default function About() {
   return (
     <section className="p-4">
       <div>
-        <div className="py-4">
-          <div className="flex gap-[11.33px]">
-            <div className="flex-1 rounded-[5.85px] overflow-hidden p-2 shadow-md">
-              <div>
-                <h2 className="text-[17px] font-bold mb-3">Our Mission</h2>
-                <p className="text-[8.41px] leading-[14.5px]">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Fugiat ducimus commodi, officia nemo porro placeat soluta
-                  obcaecati corporis maiores autu.
-                </p>
-              </div>
-            </div>
-            <div className="w-[179.07px] h-[118.04px] rounded-[5.85px] flex-1 overflow-hidden">
-              <Image
-                src="/images/heroImg.jpg"
-                alt="our mission image"
-                width={179.07}
-                height={118.04}
-                className="object-contain"
-              />
-            </div>
-          </div>
+        <div className="py-4 flex flex-col gap-10">
+          {aboutCardInfo.map((card, id) => (
+            <AboutCard info={card} key={id} />
+          ))}
         </div>
       </div>
     </section>
---
+++ app/data/about.ts
@@ -0,0 +1,14 @@
+export const aboutCardInfo = [
+  {
+    title: "Our Mission",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ducimus commodi, officia nemo porro placeat soluta obcaecati corporis maiores autu.",
+    image: "heroImg.jpg",
+    direction: "row",
+  },
+  {
+    title: "Our Vission",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ducimus commodi, officia nemo porro placeat soluta obcaecati corporis maiores autu.",
+    image: "heroImg.jpg",
+    direction: "row-reverse",
+  },
+];
