6a9879a61827eeb31d6c3530cc4b77235821d948
Mubarak <adebowaleraji0103@gmail.com>
2026-01-22 13:12:04 +0100

    I'm done with the welcome board section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/dashboard/Header";
+import WelcomeBoard from "@/components/dashboard/WelcomeBoard"
 
 export default function Dashboard() {
   return (
-    <div className="px-6 py-4">
+    <div className="px-6 py-4 flex flex-col gap-9">
       <Header />
+      <WelcomeBoard/>
     </div>
   );
 }
